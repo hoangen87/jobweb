1fb1f0b3575cd30b38176eef9c31e3271dc67a7b
Hoang Yen <yh21071987@gmail.com>
2026-07-19 15:44:15 +0700

    Fix database migration, dung db push thay vi migrate deploy

--- prisma/migrations/20260716113320_add_application_screening_fields/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Application" ADD COLUMN "dateOfBirth" TIMESTAMP(3);
-ALTER TABLE "Application" ADD COLUMN "education" TEXT;
-ALTER TABLE "Application" ADD COLUMN "experienceYears" INTEGER;
-ALTER TABLE "Application" ADD COLUMN "fieldOfExpertise" TEXT;
